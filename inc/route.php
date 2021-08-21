8773dd5696cd5b59d2f314610d5440ad9e2168ef
Stuart Carazo <stuartcarazo@gmail.com>
2021-08-21 02:43:56 -0600

    Configuración rutas

---
+++ inc/route.php
@@ -0,0 +1,22 @@
+<?php
+
+function loadController($controller = DEFAULT_CONTROLLER){
+    $controllerName = ucfirst($controller).'Controller';
+    $controllerFile = 'Controllers/'.ucfirst($controller).'.php';
+
+    if(!is_file($controllerFile)){
+        $controllerName = ucfirst(DEFAULT_CONTROLLER).'Controller';
+        $controllerFile = 'Controllers/'.DEFAULT_CONTROLLER.'.php';
+    }
+
+    require_once $controllerFile;
+    $controller = new $controllerName();
+    return $controller;
+}
+
+function loadAction($controller, $action = DEFAULT_ACTION){
+    if (method_exists($controller,$action))
+        $controller->$action();
+    else
+        $controller->DEFAULT_ACTION();
+}
